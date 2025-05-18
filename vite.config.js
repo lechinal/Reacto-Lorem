5454c384575de885f0af2523ae29b2faa36ba513
Alin Lechintan <alin.lechintan@outlook.com>
2025-05-18 23:14:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lechinal.github.io/Reacto-Lorem/",
+  base: "https://lechinal.github.io/Reacto-Lorem",
 });
