70ba01bbf7c6ac660e7cd5302c6662df9f6d28fb
Alin Lechintan <122990717+lechinal@users.noreply.github.com>
2025-10-15 10:08:37 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
-import styles from "../Logo/Logo.module.css"; // Importă stilurile CSS pentru componenta Logo
+import styles from "../Logo/Logo.module.css"; 
 
 import LogoSvgFile from "../../assets/logo.svg?react";
 
 function Logo(props) {
   const { className, ...restProps } = props;
   return (
-    <LogoSvgFile // Folosește direct componenta importată
+    <LogoSvgFile 
       className={`${styles.logoDefault} ${className || ""}`}
       aria-hidden="true"
       focusable="false"
