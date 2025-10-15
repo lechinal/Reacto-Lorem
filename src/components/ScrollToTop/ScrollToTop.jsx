b92fa24980c1b98f772e8103a70202a7f7b97763
Alin Lechintan <122990717+lechinal@users.noreply.github.com>
2025-10-15 10:10:05 +0300

    Update ScrollToTop.jsx

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation(); // Destructurează pathname din obiectul location
+  const { pathname } = useLocation(); 
+  // Destructureaza pathname din obiectul location
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Derulează la coordonatele (0, 0) - vârful paginii
-  }, [pathname]); // Rulează acest efect de fiecare dată când pathname se schimbă
+    window.scrollTo(0, 0); // Deruleaza la coordonatele (0, 0) - varful paginii
+  }, [pathname]); // Ruleaza acest efect de fiecare dată când pathname se schimbă
 
-  return null; // Această componentă nu randează nimic vizibil
+  return null; // Această componenta nu randează nimic vizibil
 }
 
 export default ScrollToTop;
